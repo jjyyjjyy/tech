35bed9e8fc8f3fa1f0b2eef7b36bce5e8d7f9575
jy <977996067@qq.com>
2017-11-07 20:45:53 +0800

    Add serialize Tests

---
+++ basic-knowledge/src/test/java/me/jy/se/lang/SerializationTests.java
@@ -0,0 +1,64 @@
+package me.jy.se.lang;
+
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * @author jy
+ * @date 2017/11/07
+ */
+public class SerializationTests implements Serializable {
+
+    private static final long serialVersionUID = 989148841462074401L;
+
+    @Test
+    public void testSerialize() throws Exception {
+        Shaker shaker = new Shaker();
+        shaker.setName("wow");
+
+        String fileName = "shake.dat";
+
+        ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(fileName));
+        outputStream.writeObject(shaker);
+        outputStream.flush();
+        outputStream.close();
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fileName));
+//        assertEquals("wow", Shaker.class.cast(ois.readObject()).getName());
+
+        new File(fileName).delete();
+
+        ois.close();
+    }
+
+    public static class Shaker implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Shaker{" + "name='" + name + "'}";
+        }
+
+        private void readObject(ObjectInputStream in) throws IOException,
+                ClassNotFoundException {
+            // called when deserialize
+//            throw new InvalidObjectException("readObject - can't deserialize Shaker");
+        }
+
+        private void readObjectNoData() throws ObjectStreamException {
+            throw new InvalidObjectException("readObjectNoData - can't deserialize Shaker");
+        }
+    }
+}
