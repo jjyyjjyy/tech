5bb4984d81380d1e96b4339f829bab9d979bb5b9
jy <977996067@qq.com>
2017-11-11 15:17:54 +0800

    Add Retention&ElementType tests

--- basic-knowledge/src/test/java/me/jy/se/lang/AnnotationTests.java
+++ basic-knowledge/src/test/java/me/jy/se/lang/AnnotationTests.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * @author jy
@@ -29,6 +30,26 @@ public class AnnotationTests {
 
     }
 
+    @Test
+    public void testRetention() {
+        assertEquals(null, SourceAnnotationTester.class.getAnnotation(SourceAnnotation.class));
+        assertEquals(null, ClassAnnotationTester.class.getAnnotation(ClassAnnotation.class));
+        // TODO: RuntimeAnnotation, $Proxy6
+        assertNotEquals(RuntimeAnnotation.class.getSimpleName(),
+                RuntimeAnnotationTester.class.getAnnotations()[0].getClass().getSimpleName());
+    }
+
+    @Test
+    public void testElementType() {
+        assertEquals(Bar.class.getTypeName(),
+                Foo.class.getAnnotatedSuperclass().getType().getTypeName());
+
+        assertEquals("BBB",
+                Foo.class.getAnnotatedSuperclass().getAnnotation(TypeUse.class).value());
+    }
+
+    // Classes for test @Repeatable
+
     @Repeatable(MiracleContainer.class)
     @Target({ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
     @Retention(RetentionPolicy.RUNTIME)
@@ -42,6 +63,61 @@ public class AnnotationTests {
     @interface MiracleContainer {
 
         Miracle[] value();
+    }
+
+    // Classes for test Retention
+
+    @ClassAnnotation
+    private static class ClassAnnotationTester {
+    }
+
+    @Retention(RetentionPolicy.CLASS)
+    @Target(ElementType.TYPE)
+    private @interface ClassAnnotation {
+
+    }
+
+    /**
+     * EQ:
+     * private static class SourceAnnotationTester {
+     * private SourceAnnotationTester() {
+     * }
+     * }
+     */
+    @SourceAnnotation
+    private static class SourceAnnotationTester {
+    }
+
+
+    @Retention(RetentionPolicy.SOURCE)
+    @Target(ElementType.TYPE)
+    private @interface SourceAnnotation {
+
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    private @interface RuntimeAnnotation {
+
+    }
+
+    @RuntimeAnnotation
+    private static class RuntimeAnnotationTester {
+    }
+
+
+    // Classes for test ElementType
+
+    @Target(ElementType.TYPE_USE)
+    @Retention(RetentionPolicy.RUNTIME)
+    private @interface TypeUse {
+        String value() default "";
+    }
+
+    private static class Foo extends @TypeUse("BBB") Bar {
+    }
+
+    private static class Bar {
     }
 
 }
