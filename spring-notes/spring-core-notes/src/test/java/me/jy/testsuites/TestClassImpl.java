d212b30399742d4d71414dc6d58d196fc5fcd3be
jy <977996067@qq.com>
2018-02-13 16:09:42 +0800

    Remove unused module

--- spring-notes/spring-core-notes/src/test/java/me/jy/testsuites/TestClassImpl.java
+++
@@ -1,8 +0,0 @@
-package me.jy.testsuites;
-
-/**
- * @author jy
- * @date 2018/01/21
- */
-public class TestClassImpl implements TestInterfaceA, TestInterfaceB {
-}
--- spring-notes/spring-core-notes/src/test/java/me/jy/testsuites/TestSuperGenericObject.java
+++
@@ -1,9 +0,0 @@
-package me.jy.testsuites;
-
-/**
- * @author jy
- * @date 2018/01/21
- */
-public abstract class TestSuperGenericObject<T> {
-    protected abstract T get(T t);
-}
