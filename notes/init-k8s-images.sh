20648ab30a606d5d78ed1633d6d6ea4bb2f7a420
jy <977996067@qq.com>
2018-08-12 15:32:24 +0800

    :sparkles: Add vagrant notes

--- notes/init-k8s-images.sh
+++ notes/init-k8s-images.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-images=(kube-proxy-amd64:v1.11.1 kube-scheduler-amd64:v1.11.1 kube-controller-manager-amd64:v1.11.1 kube-apiserver-amd64:v1.11.1 etcd-amd64:3.2.18 pause:3.1 coredns:1.1.3)
+
+KUBE_VER="v1.11.2"
+images=(
+kube-proxy-amd64:`${KUBE_VER}`
+kube-scheduler-amd64:`${KUBE_VER}`
+kube-controller-manager-amd64:`${KUBE_VER}`
+kube-apiserver-amd64:`${KUBE_VER}`
+etcd-amd64:3.2.18
+pause:3.1
+coredns:1.1.3
+)
 for imageName in ${images[@]} ; do
   docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
   docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
