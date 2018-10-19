176b2bd8d83602c706e4d1f1f6ac76d48b89da27
jy <977996067@qq.com>
2018-10-19 09:51:43 +0800

    :arrow_up: Upgrade k8s note to v1.12.1

--- notes/sh/k8s-install.sh
+++ notes/sh/k8s-install.sh
@@ -11,25 +11,41 @@ sudo sed -i 's/ExecStart=\/usr\/bin\/kubelet.*/& $KUBE_PAUSE/g' /etc/systemd/sys
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
 
+#!/usr/bin/env bash
 echo "====== pull images now ======"
 
-KUBE_VER="v1.12.0"
+KUBE_VER="v1.12.1"
 IMAGE_MIRROR="registry.cn-hangzhou.aliyuncs.com/google_containers"
-images=(
-kube-proxy-amd64:$KUBE_VER
-kube-scheduler-amd64:$KUBE_VER
-kube-controller-manager-amd64:$KUBE_VER
-kube-apiserver-amd64:$KUBE_VER
-etcd-amd64:3.2.24
-pause:3.1
-coredns:1.2.2
-)
-for imageName in ${images[@]} ; do
-  docker pull ${IMAGE_MIRROR}/${imageName}
-  docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/${imageName}
-  docker rmi ${IMAGE_MIRROR}/${imageName}
-done
+
+imageName="kube-proxy-amd64:$KUBE_VER"
+docker pull ${IMAGE_MIRROR}/${imageName}
+docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/kube-proxy:$KUBE_VER
+docker rmi ${IMAGE_MIRROR}/${imageName}
+imageName="kube-scheduler-amd64:$KUBE_VER"
+docker pull ${IMAGE_MIRROR}/${imageName}
+docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/kube-scheduler:$KUBE_VER
+docker rmi ${IMAGE_MIRROR}/${imageName}
+imageName="kube-controller-manager-amd64:$KUBE_VER"
+docker pull ${IMAGE_MIRROR}/${imageName}
+docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/kube-controller-manager:$KUBE_VER
+docker rmi ${IMAGE_MIRROR}/${imageName}
+imageName="kube-apiserver-amd64:$KUBE_VER"
+docker pull ${IMAGE_MIRROR}/${imageName}
+docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/kube-apiserver:$KUBE_VER
+docker rmi ${IMAGE_MIRROR}/${imageName}
+imageName="etcd-amd64:3.2.24"
+docker pull ${IMAGE_MIRROR}/${imageName}
+docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/etcd:3.2.24
+docker rmi ${IMAGE_MIRROR}/${imageName}
+imageName="pause:3.1"
+docker pull ${IMAGE_MIRROR}/${imageName}
+docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/${imageName}
+docker rmi ${IMAGE_MIRROR}/${imageName}
+imageName="coredns:1.2.2"
+docker pull ${IMAGE_MIRROR}/${imageName}
+docker tag ${IMAGE_MIRROR}/${imageName} k8s.gcr.io/${imageName}
+docker rmi ${IMAGE_MIRROR}/${imageName}
 
 echo "Done."
 
-# sudo kubeadm init --kubernetes-version=1.12.0 --pod-network-cidr=10.244.0.0/16
+# sudo kubeadm init --kubernetes-version=1.12.1 --pod-network-cidr=10.244.0.0/16
