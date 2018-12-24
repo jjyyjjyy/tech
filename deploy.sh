96cf8849fe3a28a08156ee40405724fc572078e8
jy <977996067@qq.com>
2018-12-24 17:25:42 +0800

    :sparkles: Update notes

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+SITE_DIR=~/IdeaProjects/jjyyjjyy.github.io/
+CHECK_NOTHING=$(echo `git status` | grep "nothing to commit")
+
 cd notes
 mvn clean asciidoctor:process-asciidoc
 cd target/generated-docs
-mv * ~/IdeaProjects/jjyyjjyy.github.io/
-cd ~/IdeaProjects/jjyyjjyy.github.io/
-rm -rf pom.xml
-git add .
-git commit -m ":memo: Update"
-git push origin
+cp -r * ${SITE_DIR}  && cd ${SITE_DIR}
+git pull
+rm -rf pom.xml && rm -rf *.iml
+if [[ ${#CHECK_NOTHING} != 0 ]];then
+    echo "nothing to commit"
+else
+    git add --renormalize .
+    git commit -m ":memo: Update"
+    git push origin
+fi
