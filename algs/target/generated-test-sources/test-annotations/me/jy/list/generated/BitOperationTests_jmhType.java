ec0a2e47d87baa3249ffcfb9211b936fcd5ef5ba
jy <977996067@qq.com>
2019-04-17 00:06:59 +0800

    :rocket: Update gh-pages scripts

--- algs/target/generated-test-sources/test-annotations/me/jy/list/generated/BitOperationTests_jmhType.java
+++
@@ -1,4 +0,0 @@
-package me.jy.list.generated;
-public class BitOperationTests_jmhType extends BitOperationTests_jmhType_B3 {
-}
-
--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,17 @@ ORIGIN_BRANCH=gh-pages
 cd notes
 mvn clean asciidoctor:process-asciidoc
 rm -rf ${TMP_DIR} && mkdir -p ${TMP_DIR}
-cp -r target/generated-docs/ ${TMP_DIR}
+cp -r target/generated-docs/** ${TMP_DIR}
 cd ${TMP_DIR}
 git init
 git remote add origin ${ORIGIN_ADDRESS}
-git branch -D ${ORIGIN_BRANCH}
-git checkout -b ${ORIGIN_BRANCH}
 git add .
 git commit -m ":memo: Update"
+git checkout -b ${ORIGIN_BRANCH}
+git checkout -b tmp
+git branch -D ${ORIGIN_BRANCH}
+git push origin --delete ${ORIGIN_BRANCH}
+git checkout -b ${ORIGIN_BRANCH}
 git push --set-upstream origin ${ORIGIN_BRANCH}
 cd -
-echo "gh-pages sync successfully!"
+echo "${ORIGIN_BRANCH} sync successfully!"
