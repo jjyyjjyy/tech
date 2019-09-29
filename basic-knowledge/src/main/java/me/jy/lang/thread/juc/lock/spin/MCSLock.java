d8e82a7ee3709271ea4df0db60b4e28ca5d46cfc
jy <977996067@qq.com>
2019-09-29 10:28:14 +0800

    :memo: Add comment for MCS lock

--- basic-knowledge/src/main/java/me/jy/lang/thread/juc/lock/spin/MCSLock.java
+++ basic-knowledge/src/main/java/me/jy/lang/thread/juc/lock/spin/MCSLock.java
@@ -19,8 +19,8 @@ public class MCSLock implements Lock {
         Node preNode = tail.getAndSet(current);
         if (preNode != null) {
             current.locked = true;
-            preNode.next = current;
-            while (current.locked) ;
+            preNode.next = current; // 将上一个线程的next设置为该线程
+            while (current.locked) ; // 本地自旋, 等待上一个线程释放这个线程的锁
         }
     }
 
@@ -28,18 +28,22 @@ public class MCSLock implements Lock {
     public void unlock() {
         Node current = this.node.get();
         if (current.next == null) {
+            // 如果tail是自己, 则说明只有自己竞争锁, 那么直接清空tail返回.
             if (tail.compareAndSet(current, null)) {
                 return;
             }
+            // 需要等待其他线程释放锁
             while (current.next == null) ;
         }
+        // 如果next不为空, 则直接释放下一个线程的锁
         current.next.locked = false;
         current.next = null;
     }
 
 
     private static class Node {
+        // important: invalid local cache
         private volatile boolean locked;
-        private Node next;
+        private volatile Node next;
     }
 }
