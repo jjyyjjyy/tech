69ea3f3e7af3791d129316a4dccd62afd9739bd1
jy <977996067@qq.com>
2020-02-09 14:35:31 +0800

    :pencil: Update design-pattern note

--- design-pattern/src/main/java/me/jy/_4_singleton/SingletonProduct.java
+++ design-pattern/src/main/java/me/jy/_4_singleton/SingletonProduct.java
@@ -5,22 +5,18 @@ package me.jy._4_singleton;
  */
 public class SingletonProduct {
 
-    private enum S5 {
-        INSTANCE;
-
-        public void doStuff() {
-            // NO-OP
-        }
-    }
-
+    // tag::singleton-lazy-1[]
     private static class S1 {
+
         private static final S1 INSTANCE = new S1();
 
         public static S1 getInstance() {
             return INSTANCE;
         }
     }
+    // end::singleton-lazy-1[]
 
+    // tag::singleton-lazy-2[]
     private static class S2 {
         private static final S2 INSTANCE;
 
@@ -32,31 +28,59 @@ public class SingletonProduct {
             return INSTANCE;
         }
     }
+    // end::singleton-lazy-2[]
 
+    // tag::singleton-eager-1[]
     private static class S3 {
         private static volatile S3 INSTANCE;
 
-        public static S3 getInstance() {
+        public synchronized static S3 getInstance() {
             if (INSTANCE == null) {
-                synchronized (S3.class) {
+                INSTANCE = new S3();
+            }
+            return INSTANCE;
+        }
+    }
+    // end::singleton-eager-1[]
+
+    // tag::singleton-eager-2[]
+    private static class S4 {
+        private static volatile S4 INSTANCE;
+
+        public static S4 getInstance() {
+            if (INSTANCE == null) {
+                synchronized (S4.class) {
                     if (INSTANCE == null) {
-                        INSTANCE = new S3();
+                        INSTANCE = new S4();
                     }
                 }
             }
             return INSTANCE;
         }
     }
+    // end::singleton-eager-2[]
 
-    private static class S4 {
+    // tag::singleton-inner-static-class[]
+    private static class S5 {
 
-        public static S4 getInstance() {
+        public static S5 getInstance() {
             return S4Holder.INSTANCE;
         }
 
         private static class S4Holder {
-            private static S4 INSTANCE = new S4();
+            private static S5 INSTANCE = new S5();
         }
     }
+    // end::singleton-inner-static-class[]
+
+    // tag::singleton-enum[]
+    private enum S6 {
+        INSTANCE;
+
+        public void doStuff() {
+            // NO-OP
+        }
+    }
+    // end::singleton-enum[]
 
 }
--- design-pattern/src/main/java/me/jy/_6_builder/Product.java
+++ design-pattern/src/main/java/me/jy/_6_builder/Product.java
@@ -1,20 +1,47 @@
 package me.jy._6_builder;
 
-import lombok.Builder;
-
 import java.time.Instant;
 
 /**
  * @author jy
  */
-@Builder
 public class Product {
 
     private Long id;
     private String name;
     private Instant createdAt;
 
+    public static class ProductBuilder{
+
+        private Long id;
+        private String name;
+        private Instant createdAt;
+
+        public ProductBuilder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public ProductBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public ProductBuilder createdAt(Instant createdAt) {
+            this.createdAt = createdAt;
+            return this;
+        }
+
+        public Product build() {
+            Product product = new Product();
+            product.id = this.id;
+            product.name = this.name;
+            product.createdAt = this.createdAt;
+            return product;
+        }
+    }
+
     public static void main(String[] args) {
-        new Product.ProductBuilder().id(1L).name("pro").createdAt(Instant.now()).build();
+        new ProductBuilder().id(1L).name("pro").createdAt(Instant.now()).build();
     }
 }
