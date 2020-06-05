878fc47becc93a12dd5d5179489cb1f8b05788c2
jy <977996067@qq.com>
2020-06-05 13:35:02 +0800

    :pencil: Add Spring Cloud discovery demo

---
+++ framework-demo/spring-cloud-discovery-demo/src/main/java/me/jy/DiscoveryClientDemoApplication.java
@@ -0,0 +1,15 @@
+package me.jy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jy
+ */
+@SpringBootApplication
+public class DiscoveryClientDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryClientDemoApplication.class, args);
+    }
+}
