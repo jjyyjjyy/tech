1c6c26c1da8954a6da65e3a58b4cef436f920cf6
jy <977996067@qq.com>
2020-07-15 13:28:41 +0800

    :sparkles: Add multi consumers in stream app

--- framework-demo/spring-cloud-stream-demo/src/main/java/com/example/SpringCloudStreamDemoApplication.java
+++ framework-demo/spring-cloud-stream-demo/src/main/java/com/example/SpringCloudStreamDemoApplication.java
@@ -1,10 +1,13 @@
 package com.example;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 /**
@@ -15,8 +18,21 @@ import java.util.function.Consumer;
 public class SpringCloudStreamDemoApplication {
 
     @Bean
-    public Consumer<String> demoConsumer() {
-        return s -> log.info("Consume: {}", s);
+    public Consumer<String> demoConsumerA() {
+        return s -> log.info("ConsumeA: {}", s);
+    }
+
+    @Bean
+    public Consumer<String> demoConsumerB() {
+        return s -> log.info("ConsumeB: {}", s);
+    }
+
+    @Bean
+    public CommandLineRunner clr(StreamBridge streamBridge) {
+        return args -> {
+            TimeUnit.SECONDS.sleep(5L);
+            streamBridge.send("demo.topic", "I am from streamBridge");
+        };
     }
 
     public static void main(String[] args) {
