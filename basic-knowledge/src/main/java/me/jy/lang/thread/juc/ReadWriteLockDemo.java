3be4f27f72ce9d6b50ed5f42b140acd24bde2396
jy <977996067@qq.com>
2020-12-29 17:56:51 +0800

    :memo: Update JUC.adoc

--- basic-knowledge/src/main/java/me/jy/lang/thread/juc/ReadWriteLockDemo.java
+++ basic-knowledge/src/main/java/me/jy/lang/thread/juc/ReadWriteLockDemo.java
@@ -4,44 +4,27 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
+ * 测试ReentrantReadWriteLock重入锁次数最大上限.
+ * -Xss10m
+ *
  * @author jy
- * @date 2018/02/25
  */
 public class ReadWriteLockDemo {
 
     private static final ReentrantReadWriteLock LOCK = new ReentrantReadWriteLock();
-
     private final Lock readLock = LOCK.readLock();
     private final Lock writeLock = LOCK.writeLock();
 
-
-    public void r() {
-        doLock(readLock);
-    }
-
-    public void w() {
-        doLock(writeLock);
-    }
-
-    private void doLock(Lock lock) {
-        lock.lock();
-        try {
-            System.out.println(lock.getClass().getSimpleName() + " in");
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            lock.unlock();
-        }
-    }
-
     public static void main(String[] args) {
         ReadWriteLockDemo test = new ReadWriteLockDemo();
-        new Thread(test::r).start();
-        new Thread(test::w).start();
-        new Thread(test::r).start();
-        new Thread(test::r).start();
-        new Thread(test::w).start();
+        new Thread(() -> test.lock(2 ^ 16 + 1)).run();
     }
 
+    public void lock(int n) {
+        writeLock.lock();
+        if (n > 0) {
+            lock(n--);
+            writeLock.unlock();
+        }
+    }
 }
